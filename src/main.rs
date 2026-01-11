26bc03bd8b4bd2d6e4174efdca93b1b757a724ce
fakhrirrahman <fakhrirrahman7@gmail.com>
2026-01-11 19:44:57 +0700

    Implement server setup with Axum and dotenv integration; add .env file for configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::{Extension, Router};
+use dotenvy::dotenv;
+
+
+
+#[tokio::main]
+async fn main(){
+    dotenv().ok();
+
+    let app = Router::new()
+        .layer(Extension(()));
+
+    let port = std::env::var("APP_PORT")
+        .ok()
+        .and_then(|p| p.parse::<u16>().ok())
+        .unwrap_or(3001);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    println!("server berjalan di {}", addr);
+
+    //jalankan server
+    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(),
+        app).await.unwrap();
+
 }
